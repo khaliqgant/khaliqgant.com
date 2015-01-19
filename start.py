ef8c5b8cb0b41695ab8c413329bd002605ac1b57
Khaliq Gant <khaliqgant@gmail.com>
2015-01-18 20:58:41 -0500

    add basic starter files

---
+++ start.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+
+@app.route('/')
+def show_header():
+    return render_template('header.html')
+
+
+@app.route('/about')
+def about():
+    return 'This the about page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
