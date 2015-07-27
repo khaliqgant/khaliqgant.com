5857735e490288584261b51d2aa3a17dca48a786
Khaliq Gant <khaliqgant@gmail.com>
2015-07-26 21:41:13 -0400

    [WIP] start work to integrate github activities for dashboard

--- __init__.py
+++ __init__.py
@@ -1,15 +1,33 @@
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import requests
+import requests_cache
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+requests_cache.install_cache(
+    'github_cache', backend='sqlite', expire_after=180
+)
+
+
+def github():
+    """ retrieve github activity"""
+    username = 'khaliqgant'
+    url = "https://api.github.com/users/%s/events" % (username)
+    response = requests.get(url)
+    # transform this into a dict and probably utf-8 encode
+    return response
 
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    gh_activities = github()
+    return render_template('layout.html', gh_activities=gh_activities)
 
-
-@app.route('/about')
-def about():
-    return 'This the about page'
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run()
