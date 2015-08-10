5977450a52a092a42127212b22b8a84275a3c6e3
Khaliq Gant <khaliqgant@gmail.com>
2015-08-09 21:59:39 -0400

    start work to integrate and sort activities by timestamps

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 import requests
 import requests_cache
 import json
-import calendar
 import ConfigParser
 import os
+from operator import itemgetter
 
 pwd = os.environ['PWD']
 
@@ -12,6 +12,8 @@ configFilePath = '%s/config.txt' % pwd
 configParser.read(configFilePath)
 
 from flask import Flask, render_template
+from datetime import datetime
+import calendar
 
 app = Flask(__name__)
 
@@ -31,6 +33,15 @@ def UTC_to_epoch(timestamp):
     return epoch
 
 
+def iso8601_to_epoch(datestring):
+    """
+        iso8601_to_epoch - convert the iso8601 date into the unix epoch time
+        @source : https://gist.github.com/squioc/3078803
+    """
+    return calendar.timegm(datetime.strptime(
+        datestring, "%Y-%m-%dT%H:%M:%SZ").timetuple())
+
+
 def github():
     """ retrieve github activity"""
     username = 'khaliqgant'
@@ -39,6 +50,12 @@ def github():
 
     # turn the response string into a json object
     activities = json.loads(response._content)
+    print type(activities)
+    for i, ac in enumerate(activities):
+        activities[i]["type"] = "github"
+        created = ac['created_at']
+        timestamp = iso8601_to_epoch(created)
+        activities[i]["timestamp"] = timestamp
 
     return activities
 
@@ -50,7 +67,12 @@ def foursquare():
         "v=20150727" % (token)
     response = requests.get(url)
     foursquares = json.loads(response._content)
-    return foursquares['response']['checkins']['items']
+    checkins = foursquares['response']['checkins']['items']
+    for i, ch in enumerate(checkins):
+        checkins[i]["type"] = "foursquare"
+        checkins[i]["timestamp"] = ch["createdAt"]
+
+    return checkins
 
 
 def lastFM():
@@ -64,18 +86,26 @@ def lastFM():
         #print attr
         #print value
     listens = json.loads(response._content)
-    return listens['recenttracks']['track']
+    lastfms = listens['recenttracks']['track']
+    for i, lf in enumerate(lastfms):
+        lastfms[i]["type"] = "lastfm"
+        if "date" in lf:
+            lastfms[i]["timestamp"] = lf["date"]["uts"]
+        else:
+            lastfms[i]["timestamp"] = "1439170466"
+
+    return lastfms
 
 
-def sort(github, foursquare):
+def sort(github, foursquare, lastfms):
     """ sort the activities by the created_at || createdAt date """
     #print type(github)
     #print type(foursquare)
     # combine the lists
-    all = [g for f in zip(github, foursquare) for g in f]
+    all = github + foursquare + lastfms
     # now sort the lists by created_at
     #print all
-    return
+    return all
 
 
 @app.route('/')
@@ -83,10 +113,13 @@ def index():
     gh_activities = github()
     fs_activities = foursquare()
     lastfms = lastFM()
-    activities = sort(gh_activities, fs_activities)
-    #print activities
+    activities = sort(gh_activities, fs_activities, lastfms)
+    # http://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-
+    # dictionaries-by-values-of-the-dictionary-in-python
+    all = sorted(activities, key=itemgetter('timestamp'), reverse=True)
     return render_template('layout.html', gh_activities=gh_activities,
-                           fs_activities=fs_activities, lastfms=lastfms)
+                           fs_activities=fs_activities, lastfms=lastfms,
+                           all=all)
 
 
 if __name__ == '__main__':
