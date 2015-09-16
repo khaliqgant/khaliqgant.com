37def48061523a091bceb91a62fd565a69602f35
Khaliq Gant <khaliqgant@gmail.com>
2015-09-16 19:59:32 -0400

    don't cache citi bike response

--- apis/citibike.py
+++ apis/citibike.py
@@ -1,24 +1,23 @@
 import requests
+import requests_cache
 import json
-import datetime
 
 
-def retrieve(home_east,home_west,school_return):
+def retrieve(home_east, home_west, school_return):
     """ retrieve citi bike info"""
     url = "http://api.citybik.es/citi-bike-nyc.json"
-    response = requests.get(url)
-    bikes = json.loads(response._content)
-    #print(datetime.datetime.strptime( "2015-09-12T14:44:59.798Z", "%Y-%m-%dT%H:%M:%S.%fZ" ))
-    stats = {}
-    for bike in bikes:
-        if bike['idx'] == int (home_east):
-            stats['timestamp'] = bike['timestamp']
-            stats['home_east'] = str (bike['bikes'])
-        if bike['idx'] == int (home_west):
-            stats['home_west'] = str (bike['bikes'])
-        if bike['idx'] == int (school_return):
-            stats['school_return'] = str (bike['bikes'])
+    # don't cache citi bike b/c need up to date info!
+    with requests_cache.disabled():
+        response = requests.get(url)
+        bikes = json.loads(response._content)
+        stats = {}
+        for bike in bikes:
+            if bike['idx'] == int(home_east):
+                stats['timestamp'] = bike['timestamp']
+                stats['home_east'] = str(bike['bikes'])
+            if bike['idx'] == int(home_west):
+                stats['home_west'] = str(bike['bikes'])
+            if bike['idx'] == int(school_return):
+                stats['school_return'] = str(bike['bikes'])
 
     return stats
-
-
