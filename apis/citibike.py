2d6b3e564ba7fca02197066c2e1ca27282dc2ade
Khaliq Gant <khaliqgant@gmail.com>
2015-09-16 20:08:33 -0400

    use number instead of idx b/c that changes it seems

--- apis/citibike.py
+++ apis/citibike.py
@@ -12,12 +12,12 @@ def retrieve(home_east, home_west, school_return):
         bikes = json.loads(response._content)
         stats = {}
         for bike in bikes:
-            if bike['idx'] == int(home_east):
+            if bike['number'] == int(home_east):
                 stats['timestamp'] = bike['timestamp']
                 stats['home_east'] = str(bike['bikes'])
-            if bike['idx'] == int(home_west):
+            if bike['number'] == int(home_west):
                 stats['home_west'] = str(bike['bikes'])
-            if bike['idx'] == int(school_return):
+            if bike['number'] == int(school_return):
                 stats['school_return'] = str(bike['bikes'])
 
     return stats
