6d3792ece6fb2cc0ea6996123398a9c6dc3c03f5
Khaliq Gant <khaliqgant@gmail.com>
2016-03-05 22:52:05 -0500

    use os env or config for api key

--- tests/RescueTime_Test.py
+++ tests/RescueTime_Test.py
@@ -7,8 +7,10 @@ pwd = os.path.dirname(os.path.abspath(__file__))
 # put apis in package path
 sys.path.insert(0, os.path.normpath(os.path.join(pwd, '../apis')))
 import rescuetime
+sys.path.insert(0, os.path.normpath(os.path.join(pwd, '../config')))
+import auth
 
-KEY = os.environ['RESCUETIME']
+env = os.environ
 
 
 class RescueTimeTest(unittest.TestCase):
@@ -16,15 +18,34 @@ class RescueTimeTest(unittest.TestCase):
     def setUp(self):
         pass
 
+    def grabKey(self):
+        if "RESCUETIME" in env:
+            KEY = env['RESCUETIME']
+        else:
+            configParser = auth.grab()
+            KEY = configParser.get('rescuetime', 'key')
+        return KEY
+
+
     """ Make sure the expected keys are there """
-    def test_todaysProductivity(self):
+    def test_productivityResponse(self):
+        KEY = self.grabKey()
         productivity = rescuetime.todaysProductivity(KEY, True)
         self.assertEqual(
             productivity['row_headers'][1], "Time Spent (seconds)"
         )
         self.assertEqual('row_headers' in productivity, True)
         self.assertEqual('rows' in productivity, True)
-        assert 1 == 1
+
+
+    """
+        Make sure the response is formatted to what will be outputted in the
+        template
+    """
+    def test_todaysProductivity(self):
+        KEY = self.grabKey()
+        productivity = rescuetime.todaysProductivity(KEY)
+        self.assertEqual(type(productivity), type([]))
 
 
 if __name__ == "__main__":
