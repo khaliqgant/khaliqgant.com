8b00762e9b9fdd0e816d96cfebbfb25e84d03e15
Khaliq Gant <khaliqgant@gmail.com>
2016-05-08 21:13:19 -0400

    reorg and comment

--- static/js/pack/workout.js
+++ static/js/pack/workout.js
@@ -1,32 +1,22 @@
 /* jshint -W097 */
 /* jshint node: true */
-
-'use strict';
-
 /* global document */
 /* global JSONEditor */
+'use strict';
 
+
+/**
+ * @desc binds JSONEditor to the window
+ * @requires json-editor
+ */
 require('json-editor');
 
-var Schemas = requireAll(
-    require.context('json!workout-tracker/workouts', true, /^\.\/.*\.json$/)
-);
 
-var editor,
-    el = document.getElementById('editor');
+/* App ------------------------------- */
+loadSchemas();
 
-for (var i = 0; i < Schemas.length; i++)
-{
-    editor = new JSONEditor(el, {
-        ajax: true,
-        theme: 'bootstrap2',
-        collapsed: true,
-        schema: Schemas[i]
-    });
-}
 
-/* Utils -------------------------------
- *
+/* Utils ------------------------------- */
 /**
  * Require All
  * @desc grab all the files within a directory
@@ -34,4 +24,33 @@ for (var i = 0; i < Schemas.length; i++)
  */
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
+}
+
+/**
+ * Schemas
+ * @desc grab all the workout JSON schemas from the workout-tracker directory
+ * @type array
+ */
+var Schemas = requireAll(
+    require.context('json!workout-tracker/workouts', true, /^\.\/.*\.json$/)
+);
+
+/**
+ * Load Schemas
+ * @desc iterate through each workout and load it in using JSONEditor with
+ *       configuration options
+ */
+function loadSchemas() {
+    var editor,
+    el = document.getElementById('editor');
+
+    for (var i = 0; i < Schemas.length; i++)
+    {
+        editor = new JSONEditor(el, {
+            ajax: true,
+            theme: 'bootstrap2',
+            collapsed: true,
+            schema: Schemas[i]
+        });
+    }
 }
